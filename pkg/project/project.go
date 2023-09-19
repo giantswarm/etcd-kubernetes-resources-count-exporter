a91309a2461797d34ce524ffc5f16df37948ef2c
taylorbot <dev@giantswarm.io>
2023-09-19 11:27:36 +0000

    Release v1.5.0

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "etcd-kubernetes-resources-count-exporter"
 	source      string = "https://github.com/giantswarm/etcd-kubernetes-resources-count-exporter"
-	version            = "1.4.1-dev"
+	version            = "1.5.0"
 )
 
 func Description() string {
