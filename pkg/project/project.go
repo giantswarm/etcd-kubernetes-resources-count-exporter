28c657d28655920c2edc6700a34708c8634cb891
taylorbot <dev@giantswarm.io>
2025-06-03 07:59:44 +0000

    Bump version to 1.10.5-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "etcd-kubernetes-resources-count-exporter"
 	source      string = "https://github.com/giantswarm/etcd-kubernetes-resources-count-exporter"
-	version            = "1.10.4"
+	version            = "1.10.5-dev"
 )
 
 func Description() string {
